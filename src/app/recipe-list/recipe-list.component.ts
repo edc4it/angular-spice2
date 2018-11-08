93316b412e1c17981a1ff746ee022ec70508ef7e
Raphael Parree <rparree@edc4it.com>
2018-11-08 08:09:37 +0100

    solution lab 8 (part 2 async pipe)

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Recipe} from "../recipe-service/recipe";
 import {RecipeService} from "../recipe-service/recipe.service";
+import {Observable} from "rxjs";
 
 @Component({
   templateUrl: './recipe-list.component.html',
@@ -9,14 +10,13 @@ import {RecipeService} from "../recipe-service/recipe.service";
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [];
+  recipes: Observable<Recipe[]>;
 
   constructor(readonly service: RecipeService) {
   }
 
   ngOnInit() {
-    this.service.getAll()
-      .subscribe((recipes) => this.recipes=recipes);
+    this.recipes = this.service.getAll();
   }
 
 }
